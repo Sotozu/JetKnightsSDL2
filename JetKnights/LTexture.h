cd56252cb05b082317c9d305103a4c8a7994ede3
Alex <Alexander.w.soto@gmail.com>
2020-09-25 23:51:53 -0400

    Add project files.

---
+++ JetKnights/LTexture.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include <cmath>
+
+class LTexture
+{
+public:
+	//Initializes variables
+	LTexture();
+
+
+	//Deallocates memory
+	~LTexture();
+
+	//Loads image at specified path
+	bool loadFromFile(std::string path, SDL_Renderer* gRenderer);
+
+#if defined(SDL_TTF_MAJOR_VERSION)
+	//Creates image from font string
+	bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
+#endif
+
+	//Deallocates texture
+	void free();
+
+	//Set color modulation
+	void setColor(Uint8 red, Uint8 green, Uint8 blue);
+
+	//Set blending
+	void setBlendMode(SDL_BlendMode blending);
+
+	//Set alpha modulation
+	void setAlpha(Uint8 alpha);
+
+	//Renders texture at given point
+	void render(int x, int y, SDL_Rect* clip , SDL_Renderer* gRenderer, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	//Gets image dimensions
+	int getWidth();
+	int getHeight();
+
+private:
+	//The actual hardware texture
+	SDL_Texture* mTexture;
+
+	//Image dimensions
+	int mWidth;
+	int mHeight;
+};
---
+++ JetKnights/my_object.cpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "my_object.h"
